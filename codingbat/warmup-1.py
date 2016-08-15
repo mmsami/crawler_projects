1c4d17dcbc60ba3cb2330f037238bf27155ad28d
msami <muntasir.sami@gmail.com>
2016-08-15 15:32:59 +0600

    new practice code

--- codingbat/warmup-1.py
+++ codingbat/warmup-1.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 def diff21(n):
     """Given an int n, return the absolute difference between n and 21,
     except return double the absolute difference if n is over 21"""
@@ -7,5 +8,18 @@ def diff21(n):
         return (n - 21) * 2
 
 
+def pos_neg(a, b, negative):
+    """Given 2 int values, return True if one is negative and one is positive.
+    Except if the parameter "negative" is True, then return True only if both are negative.
+    pos_neg(1, -1, False) → True
+    pos_neg(-1, 1, False) → True
+    pos_neg(-4, -5, True) → True"""
+    if negative:
+        return a < 0 and b < 0
+    else:
+        return (a < 0 and b > 0) or (a > 0 and b < 0)
+
+
 if __name__ == '__main__':
     print diff21(23)
+    print pos_neg(-4, -5, True)
