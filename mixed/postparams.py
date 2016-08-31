be0d24185d6ae7bfbc23c59fd820f05c61be7cd5
msami <muntasir.sami@gmail.com>
2016-08-31 18:22:14 +0600

    post with keep-alive and user-agent

--- mixed/postparams.py
+++ mixed/postparams.py
@@ -3,6 +3,8 @@ import requests
 s = requests.Session()
 user_agent = {
     'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+# response = s.post("https://web3.ncaa.org/hsportal/exec/hsAction", headers=user_agent,
+#                   data={'hsActionSubmit': 'Search', 'hsCode': '010170'})
 response = s.post("https://web3.ncaa.org/hsportal/exec/hsAction", headers=user_agent,
-                  data={'hsActionSubmit': 'Search', 'hsCode': '010170'})
+                  data={'hsActionSubmit': 'Search', 'state': 'MA'})
 print response.text
